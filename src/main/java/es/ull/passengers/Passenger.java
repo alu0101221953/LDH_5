08fc8d9643a5f9db49a260306e8f8fa22c995adc
alu0101221953 <72502501+alu0101221953@users.noreply.github.com>
2023-12-16 15:04:59 +0000

    Update Passenger.java

--- src/main/java/es/ull/passengers/Passenger.java
+++ src/main/java/es/ull/passengers/Passenger.java
@@ -27,13 +27,40 @@ import java.util.regex.Pattern;
 
 import es.ull.flights.Flight;
 
+/**
+ * Clase que representa un pasajero
+ * 
+ * @since 1.0.0
+ * @version 1.0.0
+ */
 public class Passenger {
 
+    // Comentarios tipo doxygen para generar la documentación
+
+    /**
+     * Identificador del pasajero
+     */
     private String identifier;
+    /**
+     * Nombre del pasajero
+     */
     private String name;
+    /**
+     * Código de país del pasajero
+     */
     private String countryCode;
+    /**
+     * Vuelo del pasajero
+     */
     private Flight flight;
 
+    /**
+     * Constructor
+     * 
+     * @param identifier Identificador del pasajero
+     * @param name Nombre del pasajero
+     * @param countryCode Código de país del pasajero
+     */
     public Passenger(String identifier, String name, String countryCode) {
         if (!Arrays.asList(Locale.getISOCountries()).contains(countryCode)) {
             throw new RuntimeException("Invalid country code");
@@ -44,22 +71,47 @@ public class Passenger {
         this.countryCode = countryCode;
     }
 
+    /**
+     * Getter del identificador
+     * 
+     * @return Identificador del pasajero
+     */
     public String getIdentifier() {
         return identifier;
     }
 
+    /**
+     * Getter del nombre
+     * 
+     * @return Nombre del pasajero
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Getter del código de país
+     * 
+     * @return Código de país del pasajero
+     */
     public String getCountryCode() {
         return countryCode;
     }
 
+    /**
+     * Getter del vuelo
+     * 
+     * @return Vuelo del pasajero
+     */
     public Flight getFlight() {
         return flight;
     }
 
+    /**
+     * Función que une un pasajero a un vuelo
+     * 
+     * @param flight Vuelo al que unir el pasajero
+     */
     public void joinFlight(Flight flight) {
         Flight previousFlight = this.flight;
         if (null != previousFlight) {
@@ -75,10 +127,21 @@ public class Passenger {
         }
     }
 
+    /**
+     * Setter del vuelo
+     * 
+     * @param flight Vuelo del pasajero
+     */
     public void setFlight(Flight flight) {
         this.flight = flight;
     }
 
+    /**
+     * Función que comprueba si dos pasajeros son iguales
+     * 
+     * @param obj Pasajero a comparar
+     * @return true si son iguales
+     */
     @Override
     public String toString() {
         return "Passenger " + getName() + " with identifier: " + getIdentifier() + " from " + getCountryCode();
