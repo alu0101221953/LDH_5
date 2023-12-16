1c2a0267af8516701b7bccc4541277ad0c0c07f2
alu0101221953 <72502501+alu0101221953@users.noreply.github.com>
2023-12-16 15:04:48 +0000

    Update Flight.java

--- src/main/java/es/ull/flights/Flight.java
+++ src/main/java/es/ull/flights/Flight.java
@@ -27,15 +27,38 @@ import java.util.regex.Pattern;
 
 import es.ull.passengers.Passenger;
 
+/**
+ * Clase que representa un vuelo
+ * 
+ * @since 1.0.0
+ * @version 1.0.0
+ */
+
 public class Flight {
 
+    /**
+     * Número de vuelo
+     */
     private String flightNumber;
+    /**
+     * Número de asientos
+     */
     private int seats;
+    /**
+     * Pasajeros
+     */
     private Set<Passenger> passengers = new HashSet<>();
+
 
     private static String flightNumberRegex = "^[A-Z]{2}\\d{3,4}$";
     private static Pattern pattern = Pattern.compile(flightNumberRegex);
 
+    /**
+     * Constructor
+     * 
+     * @param flightNumber Número de vuelo
+     * @param seat Número de asientos
+     */
     public Flight(String flightNumber, int seat) {
         Matcher matcher = pattern.matcher(flightNumber);
         if (!matcher.matches()) {
@@ -48,18 +71,39 @@ public class Flight {
         this.seats = seat;
     }
 
+    /**
+     * Getter del número de vuelo
+     * 
+     * @return Número de vuelo
+     */
     public String getFlightNumber() {
         return flightNumber;
     }
 
+    /**
+     * Getter del número de asientos
+     * 
+     * @return Número de asientos
+     */
     public int getSeats() {
         return seats;
     }
 
+    /**
+     * Getter de los pasajeros
+     * 
+     * @return Pasajeros
+     */
     public int getNumberOfPassengers() {
         return passengers.size();
     }
 
+    /**
+     * Función que añade un pasajero
+     * 
+     * @param passenger Pasajero
+     * @return true si se ha añadido correctamente
+     */
     public boolean addPassenger(Passenger passenger) {
         if (getNumberOfPassengers() >= seats) {
             throw new RuntimeException("Not enough seats for flight " + getFlightNumber());
@@ -68,6 +112,12 @@ public class Flight {
         return passengers.add(passenger);
     }
 
+    /**
+     * Función que elimina un pasajero
+     * 
+     * @param passenger Pasajero
+     * @return true si se ha eliminado correctamente
+     */
     public boolean removePassenger(Passenger passenger) {
         passenger.setFlight(null);
         return passengers.remove(passenger);
